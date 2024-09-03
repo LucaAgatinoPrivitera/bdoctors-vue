0cc34cd67e8db09434a04b24d50265cc83d66462
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-09-03 11:40:21 +0200

    inizio e fine

--- src/components/DoctorList.vue
+++ src/components/DoctorList.vue
@@ -5,10 +5,10 @@
         <div v-else-if="error">Errore: {{ error }}</div>
         <ul v-else>
             <li v-for="doctor in doctors" :key="doctor.id">
-                <h2>{{ doctor.surname }}</h2>
-                <p>Indirizzo: {{ doctor.address }}</p>
+                <h2>Nome {{ doctor.surname }}</h2>
+                <!-- <p>Indirizzo: {{ doctor.address }}</p>
                 <p>Telefono: {{ doctor.phone }}</p>
-                <p>Bio: {{ doctor.bio }}</p>
+                <p>Bio: {{ doctor.bio }}</p> -->
                 <img v-if="doctor.pic" :src="'/storage/images/' + doctor.pic" alt="Immagine del dottore" width="100">
             </li>
         </ul>
@@ -24,19 +24,29 @@ export default {
             doctors: [],
             loading: true,
             error: null,
+            page: 1,
+            perPage: 10
         };
     },
     async created() {
-        try {
-            const response = await axios.get('/api/doctors');
-            this.doctors = response.data;
-        } catch (error) {
-            this.error = 'Errore nel recupero dei dati.';
-        } finally {
-            this.loading = false;
-        }
+        await this.fetchDoctors();
     },
-};
+    methods: {
+        async fetchDoctors() {
+            try {
+                const response = await axios.get(`/api/doctors?page=${this.page}&perPage=${this.perPage}`);
+                console.log('Dati dei dottori:', response.data); // Log dei dati
+                this.doctors = response.data.data; // Assumi che i dati siano in `data`
+            } catch (error) {
+                console.error('Errore:', error); // Log dell'errore
+                this.error = 'Errore nel recupero dei dati.';
+            } finally {
+                this.loading = false;
+            }
+        }
+    }
+}
+    ;
 </script>
 
 <style scoped>
