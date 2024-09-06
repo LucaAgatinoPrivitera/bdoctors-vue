09e7242aefa9ab4cd6dd8eedb50501922161d3ef
GuglielmoConsolini <consoliniguglielmo@gmail.com>
2024-09-06 12:52:51 +0200

    recensioni ok

--- src/views/Review.vue
+++ src/views/Review.vue
@@ -4,18 +4,18 @@
         <form @submit.prevent="submitReview">
             <div class="form-group">
                 <label for="name">Nome</label>
-                <input type="text" id="name" v-model="form.name" class="form-control" required>
+                <input type="text" id="name" v-model="form.name_reviewer" class="form-control" required>
             </div>
             <div class="form-group">
                 <label for="rating">Voto</label>
-                <select id="rating" v-model="form.rating" class="form-control" required>
+                <select id="rating" v-model="form.stars" class="form-control" required>
                     <option value="">Seleziona un voto</option>
                     <option v-for="n in 5" :key="n" :value="n">{{ n }}</option>
                 </select>
             </div>
             <div class="form-group">
                 <label for="review">Recensione</label>
-                <textarea id="review" v-model="form.review" class="form-control" required></textarea>
+                <textarea id="review" v-model="form.review_text" class="form-control" required></textarea>
             </div>
             <button type="submit" class="btn btn-primary">Invia</button>
         </form>
@@ -29,21 +29,30 @@ export default {
     data() {
         return {
             form: {
-                name: '',
-                rating: '',
-                review: ''
+                name_reviewer: '',
+                stars: '',
+                review_text: '',
+                doctor_id: '', // Inizializza anche il doctor_id
             }
         };
     },
     methods: {
         async submitReview() {
+            // Associa l'ID del dottore dal route params
+            this.form.doctor_id = this.$route.params.doctorId;
+            console.log(this.form); // Verifica i dati inviati
             try {
-                await axios.post(`/api/doctors/${this.$route.params.doctorId}/reviews`, this.form);
+                await axios.post(`http://localhost:8000/api/reviews`, this.form);
                 alert('Recensione inviata con successo!');
                 this.$router.push('/');
             } catch (error) {
-                console.error('Errore nell\'invio della recensione:', error);
-                alert('Errore nell\'invio della recensione.');
+                if (error.response) {
+                    console.error('Errore nella risposta:', error.response.data);
+                    alert(`Errore: ${JSON.stringify(error.response.data.errors)}`);
+                } else {
+                    console.error('Errore:', error);
+                    alert('Errore sconosciuto.');
+                }
             }
         }
     }
