e946187406793b7d10fe42fc5ac2a59e5b066574
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-09-08 15:23:03 +0200

    Il search adesso ha anche lo show del singolo medico

--- src/views/FilteredDoctor.vue
+++ src/views/FilteredDoctor.vue
@@ -4,12 +4,43 @@
         <div v-if="loading">Caricamento...</div>
         <div v-else-if="error">Errore: {{ error }}</div>
         <div v-else>
-            <div v-for="doctor in filteredDoctors" :key="doctor.id">
-                <!-- Visualizza i dettagli del dottore -->
-                <div>
-                    <h2>{{ doctor.surname }}</h2>
-                    <!-- Altri dettagli del dottore -->
+
+            <!-- Visualizza i dettagli del dottore -->
+            <div>
+                <div class="container">
+                    <div class="row text-dark">
+                        <div class="col-12 col-md-6 col-lg-4 py-2" v-for="doctor in filteredDoctors" :key="doctor.id">
+                            <div class="border p-3 bg-light rounded d-flex flex-column h-100 justify-content-between">
+                                <div class="d-flex justify-content-center mb-3">
+                                    <img v-if="doctor.pic" :src="`${base_url}/storage/images/${doctor.pic}`"
+                                        alt="Immagine del dottore" class="img-fluid" @error="handleImageError" />
+                                    <img v-else
+                                        src="https://i.pinimg.com/736x/ac/67/4d/ac674d2be5f98abf1c189c75de834155.jpg"
+                                        alt="Immagine del dottore" class="img-fluid" />
+                                </div>
+                                <h2>{{ doctor.surname }}</h2>
+                                <p>Indirizzo: {{ doctor.address }}</p>
+                                <p>Telefono: {{ doctor.phone }}</p>
+                                <p>Bio: {{ doctor.bio }}</p>
+
+
+
+                                <h3 v-if="doctor.specializations && doctor.specializations.length > 0">
+                                    Specializzazioni:
+                                </h3>
+                                <ul v-if="doctor.specializations">
+                                    <li v-for="specialization in doctor.specializations" :key="specialization.id">
+                                        {{ specialization.name }}
+                                    </li>
+                                </ul>
+                                <button class="btn btn-info mt-2" @click="goToDoctorDetail(doctor.slug)">
+                                    Visualizza Dettagli
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+                <!-- Altri dettagli del dottore -->
             </div>
         </div>
     </div>
@@ -59,6 +90,10 @@ export default {
             } finally {
                 this.loading = false;
             }
+        },
+        goToDoctorDetail(doctor) {
+            console.log(doctor)
+            this.$router.push({ name: 'doctorDetail', params: { slug: doctor } });
         },
         async fetchSpecializations() {
             try {
